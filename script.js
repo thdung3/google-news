2b065c58161c69107f9c9c5868b42af0c6e171e2
Dung DANG <thdung3@gmail.com>
2020-07-16 23:11:17 +0700

    update tag source

--- script.js
+++ script.js
@@ -2,16 +2,9 @@ let pageSize = 20
 let catelogy = ''
 let keyWord = ''
 const apiKey = 'e65d651066b04c3db78dc9e47be77573'
-
-document.getElementById('inputSearch').addEventListener("keyup", function (event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-        // Cancel the default action, if needed
-        event.preventDefault();
-        // Trigger the button element with a click
-        document.getElementById("btnSeach").click();
-    }
-});
+let source = {}
+let arrayResult = []
+let keySources = []
 
 const initialize = () => {
     console.log('initialize')
@@ -22,23 +15,34 @@ const initialize = () => {
 
 const callApi = async () => {
     let url = `http://newsapi.org/v2/top-headlines?country=us&pageSize=${pageSize}&apiKey=${apiKey}`
-    console.log('catelogy:', catelogy)
-    if (catelogy > '' && keyWord > '') {
-        url = `http://newsapi.org/v2/top-headlines?country=us&pageSize=${pageSize}&category=${catelogy}&q=${keyWord}&apiKey=${apiKey}`
-    } else if (catelogy > '') {
+    if (catelogy > '') {
         url = `http://newsapi.org/v2/top-headlines?country=us&pageSize=${pageSize}&category=${catelogy}&apiKey=${apiKey}`
-    } else if (keyWord > '') {
-        url = `http://newsapi.org/v2/top-headlines?country=us&pageSize=${pageSize}&q=${keyWord}&apiKey=${apiKey}`
+    }
+    if (keyWord > '') {
+        url = `http://newsapi.org/v2/everything?pageSize=${pageSize}&q=${keyWord}&apiKey=${apiKey}`
     }
     console.log('url:', url)
     let data = await fetch(url)
     let result = await data.json();
-    console.log('result:', result)
-    render(result.articles)
+    source = {}
+    arrayResult = result.articles
+    console.log('arrayResult:', arrayResult)
+    arrayResult.map(item => {
+        if (item.source.name != null) {
+            if (item.source.name in source) {
+                source[item.source.name]++
+            } else {
+                source[item.source.name] = 1
+            }
+        }
+    })
+    keySources = Object.keys(source)
+    render(arrayResult)
+    renderSource()
 }
 
-const render = (list) => {
-    let newsHTML = list.reduce((total, item) => {
+const render = (arrayList) => {
+    let newsHTML = arrayList.reduce((total, item) => {
         if (item.urlToImage == null) item.urlToImage = ''
         return total += `<div class="card mb-3">
         <div class="row no-gutters">
@@ -60,8 +64,36 @@ const render = (list) => {
     document.getElementById('listNewsArea').innerHTML = newsHTML
 }
 
+const renderSource = () => {
+    let sourceHTML = keySources.reduce((total, item) => {
+        return total += `<div class="group">
+            <input type="checkbox" onchange="searchSource()">
+            <span>${item}(${source[item]})<span>
+        </div>`
+    }, '')
+    document.getElementById('sources-item').innerHTML = sourceHTML
+}
+
+const searchSource = () => {
+    console.log('---- searchSource ----')
+    let checkBoxes = document.getElementById('sources-item').getElementsByTagName('input')
+    let arrayFiltered = arrayResult.filter(item => {
+        for (let i = 0; i < checkBoxes.length; i++) {
+            if (checkBoxes[i].checked) {
+                console.log('keySources[i]:', keySources[i])
+                if (item.source.name == keySources[i]) return item.source.name
+            }
+        }
+    })
+    console.log('arrayFiltered:', arrayFiltered)
+    if (arrayFiltered.length === 0) {
+        arrayFiltered = arrayResult;
+    }
+    render(arrayFiltered)
+}
+
 const searchCatelogy = (name) => {
-    console.log('searchCatelogy')
+    console.log('---- searchCatelogy ----')
     let key = document.getElementById('inputSearch').value
     console.log('key:', key)
     initialize()
@@ -70,12 +102,13 @@ const searchCatelogy = (name) => {
 }
 
 const searchKeyWord = () => {
-    console.log('searchKeyWord')
+    console.log('---- searchKeyWord ----')
     let key = document.getElementById('inputSearch').value
     console.log('key:', key)
     if (key > '') {
         keyWord = key
         pageSize = 20
+        document.getElementById('inputSearch').value = ''
         callApi()
     }
 }
@@ -86,3 +119,13 @@ const loadMore = () => {
 }
 
 callApi()
+
+document.getElementById('inputSearch').addEventListener("keyup", function (event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("btnSeach").click();
+    }
+});
